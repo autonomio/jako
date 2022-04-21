dd20e7adef4f8f40f7efb840d73ef3d60dc9903d
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-04-21 19:45:38 +0530

    Update docker_run.py

--- jako/docker/docker_run.py
+++ jako/docker/docker_run.py
@@ -1,2 +1,13 @@
 from .docker_pull import docker_pull
-from .docker_utils import docker_install_commands
+
+
+def docker_run(self, db_machine=False):
+    '''Run docker commands in remote machines'''
+
+    database_imagename = 'abhijithneilabraham/jako_database_docker'
+    jako_imagename = 'abhijithneilabraham/jako_docker_image'
+
+    if db_machine:
+        docker_pull(self, database_imagename)
+
+    docker_pull(self, jako_imagename)
