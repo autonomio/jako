5c0e1a2f3cb2f43e3ba57e29cc4d5f54de05857f
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-04-21 12:47:24 +0530

    pep8 fixes for __init__.py

--- jako/__init__.py
+++ jako/__init__.py
@@ -1,4 +1,5 @@
 from .distribute.DistributedScan import DistributedScan
 from .distribute.RemoteScan import RemoteScan
 from .docker.docker_pull import docker_pull
-del distribute
+
+__all__ = ['DistributedScan', 'RemoteScan', 'docker_pull']
