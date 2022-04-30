ee602ce22e81d43ada896f5569eb7b50dd5e76fe
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-05-01 01:25:17 +0530

    Graphql install scripts

---
+++ jako/experiment_status/jako_docker_graphql.sh
@@ -0,0 +1,4 @@
+# in a new directory run
+wget https://raw.githubusercontent.com/hasura/graphql-engine/stable/install-manifests/docker-compose/docker-compose.yaml
+# or run
+curl https://raw.githubusercontent.com/hasura/graphql-engine/stable/install-manifests/docker-compose/docker-compose.yaml -o docker-compose.yml
--- jako/experiment_status/tracker_utils.py
+++ jako/experiment_status/tracker_utils.py
@@ -2,17 +2,48 @@ import os
 import shutil
 
 
-def docker_compose_install_script(self):
+def create_docker_compose_install_script(self):
     filename = 'jako_docker_compose.sh'
     sh_path = os.path.dirname(__file__) + filename
     shutil.copy(sh_path, '/tmp/')
 
 
-def install_docker_compose(self, client, machine_id):
+def create_graphql_install_script(self):
+    filename = 'jako_docker_graphql.sh'
+    sh_path = os.path.dirname(__file__) + filename
+    shutil.copy(sh_path, '/tmp/')
 
-    docker_compose_install_script(self)
 
-    execute_strings = ['sh /tmp/jako_docker_compose.sh']
+def tracker_ssh_file_transfer(self, client):
+    '''Transfer the docker scripts to the remote machines'''
+
+    create_docker_compose_install_script(self)
+    create_graphql_install_script(self)
+
+    import os
+
+    sftp = client.open_sftp()
+
+    try:
+        sftp.chdir(self.dest_dir)  # Test if dest dir exists
+
+    except IOError:
+        sftp.mkdir(self.dest_dir)  # Create dest dir
+        sftp.chdir(self.dest_dir)
+
+    tracker_files = ['jako_docker_compose.sh', 'jako_docker_graphql.sh']
+
+    for file in os.listdir("/tmp/"):
+        if file in tracker_files:
+            sftp.put("/tmp/" + file, self.dest_dir + file)
+
+    sftp.close()
+
+
+def setup_graphql(self, client, machine_id):
+
+    execute_strings = ['sh /tmp/jako_docker_compose.sh',
+                       'sh /tmp/jako_docker_graphql.sh']
 
     for execute_str in execute_strings:
         stdin, stdout, stderr = client.exec_command(execute_str)
