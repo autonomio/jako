a720af5ef2421eef41619c5120c0d77ee4c36664
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-05-05 19:52:04 +0530

    run_query method

--- jako/experiment_status/tracker.py
+++ jako/experiment_status/tracker.py
@@ -1,2 +1,10 @@
-class Track:
-    pass
+import requests
+
+
+def run_query(uri, query, statusCode, headers):
+    request = requests.post(uri, json={'query': query}, headers=headers)
+    if request.status_code == statusCode:
+        return request.json()
+    else:
+        raise Exception(f'''Unexpected status code returned:
+                        {request.status_code}''')
