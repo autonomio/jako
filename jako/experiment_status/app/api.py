b09849157e040d09a1d4a89b7924afb5ed9d32ea
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-05-12 09:33:02 +0530

    add get_number_of_permutations

--- jako/experiment_status/app/api.py
+++ jako/experiment_status/app/api.py
@@ -5,7 +5,13 @@ app = FastAPI()
 tracker = Tracker()
 
 
-@app.get("/", tags=["Home"])
+@app.get("/number_of_nodes")
 def get_number_of_nodes() -> int:
     number_of_nodes = tracker.total_nodes()
     return number_of_nodes
+
+
+@app.get("/number_of_permutations")
+def get_number_of_permutations() -> int:
+    number_of_permutations = tracker.number_of_permutations()
+    return number_of_permutations
