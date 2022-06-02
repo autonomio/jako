d6834d9f082980e2108ab4a926a94fd89674f405
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-06-02 13:27:49 +0530

    added setup_db_with_graphql method

--- jako/docker/docker_utils.py
+++ jako/docker/docker_utils.py
@@ -74,6 +74,30 @@ def docker_ssh_file_transfer(self, client, db_machine=False):
     sftp.close()
 
 
+def setup_db_with_graphql(self, client, machine_id):
+
+    execute_strings = ['sh /tmp/jako_docker_compose.sh',
+                       'sudo docker compose -f /tmp/docker-compose.yml up -d'
+                       ]
+
+    for execute_str in execute_strings:
+        stdin, stdout, stderr = client.exec_command(execute_str)
+        if stderr:
+            for line in stderr:
+                try:
+                    # Process each error line in the remote output
+                    print(line)
+                except Exception as e:
+                    print(e)
+
+        for line in stdout:
+            try:
+                # Process each line in the remote output
+                print(line)
+            except Exception as e:
+                print(e)
+
+
 def docker_image_setup(self, client, machine_id, db_machine=False):
     '''Run the transmitted script remotely without args and show its output.
 
