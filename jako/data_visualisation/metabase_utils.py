f0ff64f5220eebe4bbab5357529f8cd240ad8242
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-06-15 23:32:29 +0530

    create database method in metabase_utils

--- jako/data_visualisation/metabase_utils.py
+++ jako/data_visualisation/metabase_utils.py
@@ -20,16 +20,35 @@ def get_token(self):
     return token
 
 
-def run_query(self, endpoint, req_type='get'):
+def run_query(self, token, db_host, endpoint, req_type='get'):
 
-    db_host = get_db_host(self)
     url = 'http://{}/{}'.format(db_host, endpoint)
 
-    token = get_token(self)
     headers = {'Content-Type': 'application/json', 'X-Metabase-Session': token}
     if req_type == 'get':
         res = requests.get(url, headers=headers)
     elif req_type == 'post':
         res = requests.post(url, headers=headers)
 
+    return res.json()
+
+
+def create_database(self, token, db_host):
+    headers = {'Content-Type': 'application/json', 'X-Metabase-Session': token}
+    url = 'http://{}:3000/api/database/'.format(db_host)
+    experiment_name = self.experiment_name
+
+    db = {
+            "engine": "postgres",
+            "name": experiment_name,
+            "details": {
+                "host": db_host,
+                "port": "5432",
+                "db": "postgres",
+                "user": "postgres",
+                "password": "postgres"
+            }
+            }
+
+    res = requests.post(url, headers=headers, json=db)
     return res.json()
